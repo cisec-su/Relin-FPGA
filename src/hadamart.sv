0c78d03fcd6fcb885e3239da29d02bef5578215a
berenaydogan <beren.aydogan@yahoo.com>
2024-12-11 16:52:39 +0300

    Remove redundant lines

--- src/hadamart.sv
+++ src/hadamart.sv
@@ -32,11 +32,8 @@ module hadamart
 localparam W    = LOGQ - LOGQH;
 localparam LOGT = (CORRECT) ? LOGQ : LOGQ + 1;
 
-localparam intmul_params_t intmul_params = {FF_IN, FF_MUL, FF_OUT, FF_CSA, USE_CSA};
-localparam wlm_mixed_params_t wlm_mixed_params = {LOGQ, LOGQH, CORRECT, FF_IN, FF_SUB, FF_MUL, FF_SUM, FF_OUT};
-localparam wlm_params_t wlm_params = {W, LOGQ, LOGQH, CORRECT, FF_IN, FF_SUB, FF_MUL, FF_SUM, FF_OUT};
-localparam modmul_params_t modmul_params = {W, LOGQ, LOGQH, CORRECT, FF_IN, FF_SUB, FF_MUL, FF_SUM, FF_OUT};
-localparam LAT = modmul_lat(intmul_params, wlm_mixed_params, wlm_params, modmul_params);
+localparam modmul_params_t modmul_params = {W, LOGQ, LOGQH, CORRECT, FF_IN, FF_MUL, FF_SUM, FF_SUB, FF_OUT, USE_CSA, FF_CSA};
+localparam LAT = modmul_lat(modmul_params);
 
 reg [LOGQH -1:0] qH_reg;
 
