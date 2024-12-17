1bdfb45cd039eaed4ad72e566b4b56d36b5fc259
Tolun Tosun <toluntosun@sabanciuni.edu>
2024-12-17 16:17:01 +0300

    relin hbm interface dev

---
+++ src/relin_interface.sv
@@ -0,0 +1,59 @@
+`ifndef RELIN_INTERFACE
+`define RELIN_INTERFACE
+
+interface relin_t #(
+        parameter L  = 30  , // Number of primes
+        parameter W  = 64  , // Word size
+        parameter TP = 32    // Coefficient throughput
+);
+
+  logic [$clog(L)  -1:0]    rw_id            ;
+  logic                     r_psi_inv        ;
+  logic                     r_psi_start      ;
+  logic                     r_psi_valid      ;
+  logic [W         -1:0]    r_psi    [TP-1:0]; 
+  logic                     r_psi_done       ;
+  logic [$clog(L)  -1:0]    r_poly_id        ;
+  logic                     r_poly_start     ;
+  logic                     r_poly_valid     ;
+  logic [W         -1:0]    r_poly   [TP-1:0]; 
+  logic                     r_poly_done      ;
+  logic [$clog(L)  -1:0]    r_rlk_id         ;
+  logic                     r_rlk_start      ;
+  logic                     r_rlk_valid      ;
+  logic [W         -1:0]    r_rlk_0  [TP-1:0]; 
+  logic [W         -1:0]    r_rlk_1  [TP-1:0];
+  logic                     r_rlk_done       ;
+  logic                     w_ready          ;
+  logic [W         -1:0]    w_poly   [TP-1:0];
+  logic                     w_valid          ;
+
+
+  modport master (
+    output r_idx, rw_idy,
+    output r_psi_inv, r_psi_start,
+    input  r_psi_valid, r_psi, r_psi_done,
+    output r_poly_id, r_poly_start,
+    input  r_poly_valid, r_poly, r_poly_done,
+    output r_rlk_id, r_rlk_start,
+    input  r_rlk_valid, r_rlk_0, r_rlk_1, r_rlk_done,
+    input  w_ready,
+    output w_poly, w_valid
+  );
+
+  modport slave (
+    input  r_idx, rw_idy,
+    input  r_psi_inv, r_psi_start,
+    output r_psi_valid, r_psi, r_psi_done,
+    input  r_poly_id, r_poly_start,
+    output r_poly_valid, r_poly,
+    input  r_rlk_id, r_rlk_start,
+    output r_rlk_valid, r_rlk_0, r_rlk_1, r_rlk_done,
+    output w_ready,
+    input  w_poly, w_valid
+  );
+
+
+endinterface : relin_t
+
+`endif
--- src/relin_top.sv
+++ src/relin_top.sv
@@ -1,3 +1,5 @@
+`include "relin_interface.sv"
+
 module relin_top
    #(   
         parameter L        = 30  , // Number of primes
@@ -6,40 +8,46 @@ module relin_top
         parameter TP       = 32  , // Coefficient throughput
         parameter NT       = 1024, // Number of twiddles that must be loaded
         parameter NTT_LAT  = 2000, // NTT latency
+        parameter HBM_LAT  = 10  , // HBM latency
         parameter HP_LAT   = 15  , // Hadamard product latency
         parameter ACC_LAT  = 3   , // Accum. latency
         parameter FN_LAT   = 100 , // Final op latency
     )
     (
-        input              clk              ,
-        input              rst              ,
-        input              start            ,
-        input     [W-1:0]  i_poly   [TP-1:0], 
-        input     [W-1:0]  rlk0     [TP-1:0], 
-        input     [W-1:0]  rlk1     [TP-1:0], 
-        output    [W-1:0]  o_poly   [TP-1:0],  // total bandwidth: TP*4*W
-        output    reg      o_valid
+        input              clk   ,
+        input              rst   ,
+        input              start ,
+        relin_t.master     relin_t
     );
 
 
 localparam D = 32; // bit-width for g.p. counter
 
 
-// states
-localparam ST_IDLE      = 3'd0;
-localparam ST_LOAD_Q_TW = 3'd1;
-localparam ST_NTT       = 3'd2;
-localparam ST_LOAD_ITW  = 3'd3; // load inverse twiddles
-localparam ST_INTT      = 3'd4;
+typedef enum reg[10:0] {
+    ST_IDLE        = 11'b00000000001,
+    ST_LOAD_Q      = 11'b00000000010,
+    ST_LOAD_PSI    = 11'b00000000100,
+    ST_NTT         = 11'b00000001000,
+    ST_LOAD_IPSI   = 11'b00000010000,
+    ST_INTT        = 11'b00000100000,
+} t_state;
+
+(* fsm_encoding = "none" *) t_state state;
+t_state next_state;
 
 
-
-reg [2:0] state;
-reg [2:0] next_state;
-
-reg [D-:0] ctr; // general purpose counter 
+reg [D-1:0] ctr; // general purpose counter 
 reg ctr_inc;
 reg ctr_rst;
+
+reg [$clog(L)-1:0] ctr_poly;
+reg ctr_poly_inc;
+reg ctr_poly_rst;
+
+reg [$clog(L)-1:0] ctr_rlk;
+reg ctr_rlk_inc;
+reg ctr_rlk_rst;
 
 
 reg [$clog(L)-1:0] ctr_L; // counter for L
@@ -52,7 +60,10 @@ reg  load_q;
 wire [W-1:0] ntt_in     [TP-1:0];
 wire [W-1:0] ntt_out    [TP-1:0];
 reg          intt;
-reg          load_tw;
+reg          load_psi;
+wire         ntt_out_valid;
+wire         ntt_done;
+reg          ntt_i_poly_valid;
 
 wire [W-1:0] had_in_A   [TP*2-1:0];
 wire [W-1:0] had_in_B   [TP*2-1:0];
@@ -61,9 +72,10 @@ wire [W-1:0] had_out    [TP*2-1:0];
 
 wire [W-1:0] acc_in     [TP*2-1:0];
 wire [W-1:0] acc_out    [TP*2-1:0];
-reg          acc_en;
+reg          acc_wen;
 reg          acc_sel;
 reg          acc_rst;
+reg          acc_ren;
 
 wire [W-1:0] fn_in      [TP-1:0];
 wire [W-1:0] fn_out     [TP-1:0];
@@ -90,11 +102,16 @@ ntt_wrapper #(
     .clk(clk),
     .rst(rst),
     .load_q(load_q),
-    .load_tw(load_tw),
+    .load_psi(load_psi),
     .intt(intt),
     .q(q),
-    .A_tw(ntt_in),
-    .C(ntt_out)
+    .psi_valid(relin_t.r_psi_valid),
+    .psi(relin_t.r_psi),
+    .i_poly_valid(ntt_i_poly_valid),
+    .i_poly(ntt_in),
+    .done(ntt_done),
+    .o_poly_valid(ntt_out_valid),
+    .o_poly(ntt_out)
 );
 
 
@@ -119,7 +136,8 @@ accumulator #(
 ) accumulator_inst (
     .clk(clk),
     .rst(rst | acc_rst),
-    .en(acc_en),
+    .wen(acc_wen),
+    .ren(acc_ren),
     .load_q(load_q),
     .q(q),
     .A(acc_in),
@@ -139,25 +157,25 @@ final_op #(
 
 
 
-for (genvar i = 0; i < TP; i = i + 1) begin
+// for (genvar i = 0; i < TP; i = i + 1) begin
     
-    always @(posedge clk) begin
-        ntt_out_q[i] <= ntt_out[i];
-        had_out_q[i] <= had_out[i];
-        acc_out_q[i] <= acc_out[i];
-        fn_out_q[i] <= fn_out[i];
-    end
+//     always @(posedge clk) begin
+//         ntt_out_q[i] <= ntt_out[i];
+//         had_out_q[i] <= had_out[i];
+//         acc_out_q[i] <= acc_out[i];
+//         fn_out_q [i] <= fn_out[i];
+//     end
 
-end
+// end
 
-for (genvar i = TP; i < TP*2; i = i + 1) begin
+// for (genvar i = TP; i < TP*2; i = i + 1) begin
     
-    always @(posedge clk) begin
-        had_out_q[i] <= had_out[i];
-        acc_out_q[i] <= acc_out[i];
-    end
+//     always @(posedge clk) begin
+//         had_out_q[i] <= had_out[i];
+//         acc_out_q[i] <= acc_out[i];
+//     end
 
-end
+// end
 
 
 always @(posedge clk) begin
@@ -188,6 +206,32 @@ end
 
 always @(posedge clk) begin
     if (rst) begin
+        ctr_poly <= 0;
+    end
+    else if (ctr_poly_inc) begin
+        ctr_poly <= ctr_poly + 1;
+    end
+    else if (ctr_poly_rst) begin
+        ctr_poly <= 0;
+    end
+end
+
+
+always @(posedge clk) begin
+    if (rst) begin
+        ctr_rlk <= 0;
+    end
+    else if (ctr_rlk_inc) begin
+        ctr_rlk <= ctr_rlk + 1;
+    end
+    else if (ctr_rlk_rst) begin
+        ctr_rlk <= 0;
+    end
+end
+
+
+always @(posedge clk) begin
+    if (rst) begin
         state <= ST_IDLE;
     end
     else begin
@@ -197,68 +241,114 @@ end
 
 
 for (genvar i = 0; i < TP; i = i + 1) begin
-    assign ntt_in  [i   ] = (intt)? ((acc_sel)? acc_out[i+TP]: acc_out[i]): i_poly[i];
-    assign had_in_A[i   ] = ntt_out[i   ];
-    assign had_in_A[i+TP] = ntt_out[i   ];
-    assign had_in_B[i   ] = rlk0   [i   ];
-    assign had_in_B[i+TP] = rlk1   [i   ];
-    assign acc_in  [i   ] = had_out[i   ];
-    assign acc_in  [i+TP] = had_out[i+TP];
-    assign fn_in   [i   ] = acc_out[i   ];
+    assign ntt_in  [i   ] = (intt)? ((acc_sel)? acc_out[i+TP]: acc_out[i]): relin_t.r_poly[i];
+    assign had_in_A[i   ] = ntt_out        [i   ];
+    assign had_in_A[i+TP] = ntt_out        [i   ];
+    assign had_in_B[i   ] = relin_t.r_rlk_0[i   ];
+    assign had_in_B[i+TP] = relin_t.r_rlk_1[i   ];
+    assign acc_in  [i   ] = had_out        [i   ];
+    assign acc_in  [i+TP] = had_out        [i+TP];
+    assign fn_in   [i   ] = acc_out        [i   ];
 end
+
+
+assign relin_t.rw_idy = ctr_L;
+assign relin_t.r_idx  = ctr;
 
 
 always @(*) begin
 
     intt      = 0;
-    acc_en    = 1;
+    ntt_i_poly_valid = 0;
+    acc_wen   = 1;
+    acc_ren   = 0;
     acc_sel   = 0;
     acc_rst   = 0;
     load_q    = 0;
-    load_tw   = 0;
+    load_psi  = 0;
     ctr_inc   = 0;
     ctr_rst   = 0;
     ctr_L_inc = 0;
     ctr_L_rst = 0;
+    ctr_poly_inc = 0;
+    ctr_poly_rst = 0;
+    ctr_rlk_inc  = 0;
+    ctr_rlk_rst  = 0;
     o_valid   = 0;
+
+    next_state = state;
+
+    relin_t.r_psi_inv   = 0;
+    relin_t.r_psi_start  = 0;
+    relin_t.r_poly_start = 0;
+    relin_t.r_rlk_start  = 0;
+
 
     case (state)
         ST_IDLE: begin
             if (start)
-                next_state = ST_LOAD_Q_TW;
+                next_state = ST_LOAD_Q;
             ctr_L_rst = 1;
             ctr_rst = 1;
+            ctr_poly_rst = 1;
+            ctr_rlk_rst = 1;
         end
-        ST_LOAD_Q_TW: begin
-            if (ctr == ((NT/TP)-1)) begin
-                load_tw = 1;
-                ctr_rst = 1;
-            end
-            else if (ctr == (NT/TP)) begin
-                load_q = 1;
-                next_state = ST_NTT;                
-            end
-            else begin
-                load_tw = 1;
-                ctr_inc = 1;
+        ST_LOAD_Q: begin
+            load_q = 1;
+            next_state = ST_LOAD_PSI_START_READ;
+        end
+        ST_LOAD_PSI_START_READ: begin
+            relin_t.r_psi_start = 1;
+            next_state = ST_LOAD_PSI_READ_UNTIL_DONE;
+        end
+        ST_LOAD_PSI_READ_UNTIL_DONE: begin
+            if (relin_t.psi_done) begin
+                next_state = ST_NTT_START_POLY_READ;                
             end
         end
-        ST_NTT: begin
-            if (ctr > ((NTT_LAT+HP_LAT+ACC_LAT+(L-1)*(N/TP)) - ACC_LAT - 1)) begin  // freeze accumulator
-                acc_en = 0;
+        ST_NTT_START_POLY_READ: begin
+            relin_t.r_poly_start = 1;
+            next_state = ST_NTT_HP_ACC;
+        end
+        ST_NTT_HP_ACC: begin
+            ntt_i_poly_valid = relin_t.r_poly_valid;
+
+            if (ntt_done) begin
+                relin_t.r_rlk_start = 1;
             end
 
-            if (ctr == ((NTT_LAT+HP_LAT+ACC_LAT+(L-1)*(N/TP)) - 1)) begin  // wait until all polynomials are processed
-                next_state = ST_INTT0;
-                ctr_rst = 1;
+            if (relin_t.poly_done) begin
+                if (ctr_poly < (L-1)) begin
+                    relin_t.r_poly_start = 1;
+                    ctr_poly_inc = 1; // todo: fix timing
+                end
+                else begin
+                    ctr_poly_rst = 1;
+                end
             end
-            else begin
-                ctr_inc = 1;
+
+            if (relin_t.rlk_done) begin
+                if (ctr_rlk < (L-1)) begin
+                    ctr_rlk_inc = 1; // todo: fix timing
+                end
+                else begin
+                    next_state = ST_WAIT_HP_ACC;
+                    ctr_rlk_rst = 1;
+                end
             end
         end
-        ST_LOAD_ITW: begin
-            acc_en = 0;
-            load_tw = 1;
+        ST_WAIT_HP_ACC: begin
+            if (ctr == (HP_LAT + ACC_LAT - 1)) begin  // wait until all polynomials are processed
+                next_state = ST_LOAD_IPSI;
+                ctr_rst = 1;
+            end
+            else
+                ctr_inc = 1;
+        end
+        ST_LOAD_IPSI: begin
+            relin_t.r_psi_inv = 1;
+            acc_wen = 0;
+            load_psi = 1;
             intt = 1;
             if (ctr == ((NT/TP)-1)) begin
                 ctr_rst = 1;
@@ -267,23 +357,53 @@ always @(*) begin
                 ctr_inc = 1;
             end
         end
-        ST_INTT: begin
+        ST_LOAD_IPSI_START_READ: begin
+            relin_t.r_psi_inv = 1;
+            relin_t.r_psi_start = 1;
+            next_state = ST_LOAD_PSI_READ_UNTIL_DONE;
+            acc_wen = 0;
             intt = 1;
-            acc_en = 0;
-            if (ctr > (NTT_LAT-1)) begin
-                acc_sel = 1;
+            load_psi = 1;
+        end
+        ST_LOAD_IPSI_READ_UNTIL_DONE: begin
+            acc_wen = 0;
+            intt = 1;
+            load_psi = 1;
+            if (relin_t.psi_done) begin
+                next_state = ST_INTT_START;
+                ctr_rst = 1; 
             end
-            if (ctr > (NTT_LAT+FN_LAT-1)) begin
+        end
+        ST_INTT_START: begin
+            ntt_i_poly_valid = 1;
+            intt = 1;
+            acc_wen = 0;
+            acc_ren = 1;
+            if (ctr <= ((1 << LOGN)/TP) - 1) begin
+                next_state = ST_INTT_FN;
+                ctr_rst = 1;
+            end
+            else
+                ctr_inc = 1;
+        end
+        ST_INTT_WAIT_DONE: begin
+            intt = 1;
+            if (ntt_done) begin
+                ctr_inc = 1;
+            end
+        end
+        ST_FN: begin
+            if (ctr > (FN_LAT - 1)) begin
                 o_valid = 1;
             end
-            if (ctr == (NTT_LAT+FN_LAT+(N/TP)-1)) begin
+            if (ctr == ((1 << LOGN)/TP + FN_LAT - 1)) begin
                 if (ctr_L == L) begin
                     ctr_L_rst = 1;
                     next_state = ST_IDLE;
                 end
                 else begin
                     ctr_L_inc = 1;
-                    next_state = ST_LOAD_Q_TW; // go to next prime
+                    next_state = ST_LOAD_Q; // go to next prime
                     acc_rst = 1;
                 end
                 ctr_rst = 1;
@@ -292,8 +412,6 @@ always @(*) begin
                 ctr_inc = 1;
             end
         end
-
-
     endcase
 
 
