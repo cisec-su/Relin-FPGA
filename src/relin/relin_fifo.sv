c770e83f945a8e4af82220f73af486fbb14338c4
Tolun Tosun <toluntosun@sabanciuni.edu>
2025-06-03 11:32:30 +0300

    fifo latency decreased

--- src/relin/relin_fifo.sv
+++ src/relin/relin_fifo.sv
@@ -1,34 +1,41 @@
 module relin_fifo
    #(   
         parameter K     = 1 ,
-        parameter M     = K ,
         parameter TP    = 32,
         parameter LOGQ  = 64
     )
     (
-        input              clk            ,
-        input              rst            ,
-        input              ren            , // reads for K cycles
-        input              wen            , // writes for K cycles
-        input  [LOGQ-1:0]  i_data [0:TP-1],
-        output [LOGQ-1:0]  o_data [0:TP-1] // todo: fix 1 cc delay
+        input                  clk            ,
+        input                  rst            ,
+        input                  ren            ,
+        input                  wen            ,
+        input      [LOGQ-1:0]  i_data [0:TP-1],
+        output reg [LOGQ-1:0]  o_data [0:TP-1]
     );
 
 
 localparam LOGK = $rtoi($ceil($clog2(K)));
-localparam LOGM = $rtoi($ceil($clog2(M)));
+localparam CTR_O_RST = 1;
 
 wire [LOGK-1:0] ctr_i;
 wire [LOGK-1:0] ctr_o;
 wire bram_wen;
+wire bram_ren;
+
+wire [LOGQ-1:0] bram_out [0:TP-1];
+
+wire cache_en_idata;
+wire cache_en_bram;
 
 
 assign bram_wen = wen | (|ctr_i);
+assign bram_ren = ren || (ctr_o != CTR_O_RST);
+assign cache_en_idata = (bram_wen && (ctr_i == (ctr_o - CTR_O_RST)));
+assign cache_en_bram = bram_ren;
+
 
 counter #(
-    .WIDTH(LOGK),
-    .AUTO_WIDTH(LOGM),
-    .MAX(K-1)
+    .WIDTH (LOGK)
 ) ctr_i_inst (
     .clk   (clk   ),
     .rst   (rst   ),
@@ -37,9 +44,8 @@ counter #(
 );
 
 counter #(
-    .WIDTH(LOGK),
-    .AUTO_WIDTH(LOGM),
-    .MAX(K-1)
+    .WIDTH   (LOGK     ),
+    .RST_VAL (CTR_O_RST)
 ) ctr_o_inst (
     .clk   (clk   ),
     .rst   (rst   ),
@@ -48,21 +54,32 @@ counter #(
 );
 
 
-generate
-    for (genvar i = 0; i < TP; i = i + 1) begin : BRAM_GEN
-        bram #(
-            .WIDTH (LOGQ),          // Data size (word size)
-            .LENGTH(K   )           // Memory size
-        ) bram_inst (
-            .clk  (clk          ),   // Clock signal
-            .wen  (bram_wen     ),   // Write enable
-            .waddr(ctr_i        ),   // Write address
-            .din  (i_data[i]    ),   // Data input
-            .raddr(ctr_o        ),   // Read address
-            .dout (o_data[i]    )    // Data output
-        );
+for (genvar i = 0; i < TP; i = i + 1) begin : BRAM_GEN
+    bram #(
+        .WIDTH (LOGQ),
+        .LENGTH(K   ),
+        .FF_OUT(0   )
+    ) bram_inst (
+        .clk  (clk          ),
+        .wen  (bram_wen     ),
+        .waddr(ctr_i        ),
+        .din  (i_data[i]    ),
+        .raddr(ctr_o        ),
+        .dout (bram_out[i]  )
+    );
+end
+
+
+for (genvar i = 0; i < TP; i = i + 1) begin : O_DATA_GEN
+    always @(posedge clk) begin
+        if (bram_ren) begin
+            o_data[i] <= bram_out[i];
+        end
+        else if (cache_en_idata) begin
+            o_data[i] <= i_data[i];
+        end
     end
-endgenerate
+end
 
 
 endmodule
